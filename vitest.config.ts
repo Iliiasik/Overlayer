ffc6afa54615213881fca0b2e67e8fa454cc5745
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2026-07-19 16:52:30 +0600

    Qodana setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,11 @@ import { WxtVitest } from 'wxt/testing';
 
 export default defineConfig({
   plugins: [WxtVitest()],
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text-summary', 'lcov'],
+      include: ['components/**', 'entrypoints/**', 'hooks/**', 'lib/**'],
+    },
+  },
 });
